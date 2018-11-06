1d5fcac70115f3bf78680d43a530ae59da6fcd40
rsbandhu <33539446+rsbandhu@users.noreply.github.com>
2018-11-05 21:43:47 -0800

    Add files via upload
    updated dataloader to incorporate 3 channels in the input data

--- Code/models/data_loader.py
+++ Code/models/data_loader.py
@@ -25,7 +25,7 @@ class Dataloader():
         #assert os.path.isfile(test_path), "No test file found at {}".format(test_path)
         
     
-    def load_data(self, data_type, params, ch_filter = "_green"):
+    def load_data(self, data_type, params, ch_filter = ["red", "blue", "green"]):
         """
         Reads the image file names  
         Creates a dict, key is the image count, value is the image file name without the extension
@@ -40,18 +40,28 @@ class Dataloader():
             print("datatype is train")
         elif data_type == 'test':
             current_path = params.test_data_path
-            
-        for filename in os.listdir(current_path):
-            #split the file name into name and extension
-            #filename,f_ext = os.path.splitext(os.path.join(current_path,files))
-            
-            #work with images of given filter type, green, blue, red
-            if ch_filter in filename:
-                #image_name = filename.replace(ch_filter, "")
+        
+        img_files = os.listdir(current_path)
+        file_count = len(img_files)
+        img_files_sorted = sorted(img_files)  # sort the file list by name
+        
+        for i in range(file_count//4):  # there are 4 channels
+            id_1 = img_files_sorted[i*4].split("_")[0] # split the file name by id and channel color
+            image_dict[id_1] = []
+            for j in range(4):
                 
+                id = img_files_sorted[i*4].split("_")[0]
+                channel = img_files_sorted[i*4].split("_")[1]
                 
-                image_dict[image_count] = filename
-                image_count += 1
+                if (id_1 == id):
+                    for x in ch_filter:
+                        if x in channel:
+                            image_dict[id_1].append(img_files_sorted[i*4+j])
+                else:
+                    print("image ids not matching")
+        
+            
+            image_count += 1
         return image_dict
         
     
@@ -76,7 +86,8 @@ class Dataloader():
             ytrain = np.zeros((28))
             line = line.strip()
             line_split = line.split(",") #split each line into image name and labels
-            image_name = line_split[0]+ch_filter+".png" # add the filter and .png at the end
+            image_id = line_split[0]
+            #image_name = line_split[0]+ch_filter+".png" # add the filter and .png at the end
             label = line_split[1]
             trainlabel = label.split(' ')
             
@@ -84,14 +95,14 @@ class Dataloader():
                 
                 index = int(trainlabel[j])
                 ytrain[index]=1
-            label_dict[image_name] = ytrain  #create the dict item for the image
+            label_dict[image_id] = ytrain  #create the dict item for the image
             line = label_file.readline()
 
         label_file.close()
         return label_dict
         
     
-    def data_iterator(self, params, image_dict, label_dict, shuffle = False):
+    def data_iterator(self, params, image_dict, label_dict, shuffle = True):
         """
         Returns a generator that yields batches data with labels. Batch size is params.batch_size. Expires after one
         pass over the data.
@@ -109,13 +120,14 @@ class Dataloader():
         size_X = params.im_size_X
         size_Y = params.im_size_Y
         
-        order = list(range(image_count))
+        order = list(image_dict.keys())
+        #Shuffle the order of the images if needed
         if shuffle:
-            random.seed(230)
-            random.shuffle(order)
+            np.random.seed(230)
+            np.random.shuffle(order)
         minibatch_size = params.batch_size
         
-        print( (image_count+1)//minibatch_size, minibatch_size)
+        print( image_count, (image_count+1)//minibatch_size, minibatch_size)
         # one pass over data
         for i in range((image_count+1)//minibatch_size):
             # "i" denotes batch number
@@ -123,22 +135,26 @@ class Dataloader():
             mini_batch_index_end = (i+1)*minibatch_size
             batch_image_index = order[mini_batch_index_start:mini_batch_index_end ]
             
-            # Create arrays for batch data and labels
-            batch_data = np.zeros([minibatch_size, channels, size_X, size_Y])
-            batch_labels = np.zeros([28, minibatch_size])
+            # Create initial arrays of zeros for batch data and labels
+            batch_data = torch.zeros([minibatch_size, channels, size_X, size_Y], dtype=torch.float32)
+            batch_labels = torch.zeros([28, minibatch_size], dtype=torch.float32)
             
-            # one pass over all the images in the batch
+            print("mini batch index = ", i)
+            # one pass over all the images in the mini batch
             for k in range(minibatch_size):
-                j = batch_image_index[k]
-                image_name = image_dict[j] # get the image file name
-                image_path = os.path.join(params.train_data_path,image_name)
-                img1 = misc.imread(image_path) #convert to a numpy array
+                #j = batch_image_index[k]
+                image_id = batch_image_index[k]
                 
-                batch_data[k,0,:,:] = img1
-                batch_labels[:,k] = label_dict[image_name]
+                for m in range(channels):
+                    image_name = image_dict[image_id][m] # get the image file name
+                    image_path = os.path.join(params.train_data_path,image_name)
+                    img1 = misc.imread(image_path)  #convert to a numpy array
+                
+                    batch_data[k,m,:,:] = torch.tensor(img1, dtype = torch.float32)
+                batch_labels[:,k] = torch.tensor(label_dict[image_id] , dtype = torch.float32)
             
             # since all data are indices, we convert them to torch LongTensors
-            batch_data, batch_labels = torch.LongTensor(batch_data), torch.LongTensor(batch_labels)
+            #batch_data, batch_labels = torch.HalfTensor(batch_data), torch.ShortTensor(batch_labels)
             
             # shift tensors to GPU if available
             #if params.cuda:
